35fbbf2f1c69637bd556ea387c7e24acc7e8c673
sgoblin <ryan@sgoblin.com>
2019-05-25 17:01:53 -0700

    Added toggles so parts of functionality can be turned off.

--- autoBollardCode.ino
+++ autoBollardCode.ino
@@ -9,6 +9,11 @@
 // Load cell calibration
 #define calibration_factor -10860.0 //calibrated with 750g test load
 
+// Define which parts are on
+#define LOADCELLON false
+#define TACHOON true
+#define SDON true
+
 // Change when plugged in to Arduino
 #define tachoPin 2
 #define BUTTONPIN 3
@@ -53,54 +58,65 @@ void setup() {
   while (!Serial){
     // Wait for Serial to connect
   }
-  Serial.print("Initializing SD card...");
-
-  if (!SD.begin(4)) {
-    Serial.println("initialization failed!");
-    while (1);
+  if (SDON==true){
+    Serial.print("Initializing SD card...");
+  
+    if (!SD.begin(4)) {
+      Serial.println("initialization failed!");
+      while (1);
+    }
+    Serial.println("initialization done.");
   }
-  Serial.println("initialization done.");
-
   // Button stuff
   pinMode(BUTTONPIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(BUTTONPIN), buttonPress, FALLING);
-  attachInterrupt(digitalPinToInterrupt(tachoPin), tachoChange, CHANGE);
+  if (TACHOON==true){
+    attachInterrupt(digitalPinToInterrupt(tachoPin), tachoChange, CHANGE);
+  }
 
   pinMode(INDICATORPIN, OUTPUT);
   digitalWrite(INDICATORPIN, LOW);
 
   // Load cell initialization
-  scale.begin(DOUT, CLK);
-  scale.set_scale(calibration_factor); 
-  scale.tare();  //Assuming there is no weight on the scale at start up, reset the scale to 0
-  
+  if (LOADCELLON==true){
+    scale.begin(DOUT, CLK);
+    scale.set_scale(calibration_factor); 
+    scale.tare();  //Assuming there is no weight on the scale at start up, reset the scale to 0
+  }
 }
 
 void loop() {
   if(running==true&&initialized==true&&millis()-startTime>MEASUREDELAY){
     endTime=millis();
-    double RPM = (60000.0*changes/CHANGEPERREV)/(endTime-startTime);
-  
+
     Serial.print("Time: ");
     duration=(endTime-recordTime);
-    outputFile.print(duration);
+    if (SDON==true){
+      outputFile.print(duration);
+    }
     Serial.print(duration);
-  
-    outputFile.print(",");
-  
-    Serial.print("    RPM: ");
-    Serial.print(RPM);
     
-    /*Serial.print("   changes: ");
-    Serial.println(changes);*/
-    
-    outputFile.print(RPM);
+    if (TACHOON==true){
+      double RPM = (60000.0*changes/CHANGEPERREV)/(duration);
 
-    Serial.print(scale.get_units(), 2); //"scale.get_units(), n" returns a float with n sigfigs
-    Serial.println(" N"); //Units can change but you'll need to refactor the calibration_factor
+      if (SDON==true){
+        outputFile.print(",");
+        outputFile.print(RPM);
+      }
+      
+      Serial.print("    RPM: ");
+      Serial.print(RPM);
+    }    
 
-    outputFile.print(",");
-    outputFile.println(scale.get_units());
+    if (LOADCELLON==true){
+      Serial.print(scale.get_units(), 2); //"scale.get_units(), n" returns a float with n sigfigs
+      Serial.println(" N"); //Units can change but you'll need to refactor the calibration_factor
+
+      if (SDON==true){
+        outputFile.print(",");
+        outputFile.println(scale.get_units());
+      }
+    }
 
     changes = 0;
     startTime=millis();
@@ -115,30 +131,40 @@ void buttonPress(void){
   if (millis()-oldFall>DEBOUNCETIME){
     oldFall=millis();
     if (running==true&&initialized==true){
-      // Stuff to close file, etc.
-      outputFile.close();
-      Serial.println("Closing file...");
+      if (SDON==true){
+        // Stuff to close file, etc.
+        outputFile.close();
+        Serial.println("Closing file...");
+      }
       initialized=false;
       running=false;
       digitalWrite(INDICATORPIN, LOW);
       delay(100);
     } else if (running==false&&initialized==false){
-      // Stuff to open file, etc.
-      // create a new file
-      char filename[] = "DATA00.CSV";
-      for (uint8_t i = 0; i < 100; i++) {
-        filename[4] = i/10 + '0';
-        filename[5] = i%10 + '0';
-        if (! SD.exists(filename)) {
-          // only open a new file if it doesn't exist
-          outputFile = SD.open(filename, FILE_WRITE);
-          break;  // leave the loop!
+      if (SDON==true){
+        // Stuff to open file, etc.
+        // create a new file
+        char filename[] = "DATA00.CSV";
+        for (uint8_t i = 0; i < 100; i++) {
+          filename[4] = i/10 + '0';
+          filename[5] = i%10 + '0';
+          if (! SD.exists(filename)) {
+            // only open a new file if it doesn't exist
+            outputFile = SD.open(filename, FILE_WRITE);
+            break;  // leave the loop!
+          }
         }
+        outputFile.print("Time(ms)");
+        if (TACHOON==true){
+          outputFile.print(",RPM");
+        }
+        if (LOADCELLON==true){
+          outputFile.print(",Force(N)");
+        }
+        outputFile.println("");
+        Serial.print(filename);
+        Serial.println(" successfully initialized!");
       }
-      outputFile.println("Time(ms),RPM,Force(N)");
-      //outputFile.println("RPM");
-      Serial.print(filename);
-      Serial.println(" successfully initialized!");
       digitalWrite(INDICATORPIN, HIGH);
       initialized=true;
       running=true;
@@ -147,8 +173,10 @@ void buttonPress(void){
       startTime=millis();
       recordTime=startTime;
     } else {
-      Serial.println("Something went wrong. Trying to close file.");
-      outputFile.close();
+      if (SDON==true){
+        Serial.println("Something went wrong. Trying to close file.");
+        outputFile.close();
+      }
     }
   } else {
     Serial.println("Not enough time has passed since the last event. Debouncing.");
